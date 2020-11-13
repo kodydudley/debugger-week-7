cad7436d0cea8b338cc2151564637eccbb3cf2b6
Kody Dudley <kodymdudley@gmail.com>
2020-11-13 14:58:09 -0700

    start

--- debugger-week-7.server/server/db/DbContext.js
+++ debugger-week-7.server/server/db/DbContext.js
@@ -1,9 +1,16 @@
 import mongoose from 'mongoose'
 import ValueSchema from '../models/Value'
 import ProfileSchema from '../models/Profile'
+import BugSchema from '../models/Bug'
+import NoteSchema from '../models/Note'
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
+
+  Bugs = mongoose.model('Bug', BugSchema);
+
+  Notes = mongoose.model('Note', NoteSchema);
+
   Profile = mongoose.model('Profile', ProfileSchema);
 }
 
