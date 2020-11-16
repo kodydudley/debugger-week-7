6f0beeea010d41562da08be1f4e16ab2a2f3385e
Kody Dudley <kodymdudley@gmail.com>
2020-11-15 18:46:33 -0700

    updated FE drawing bugs

--- debugger-week-7.client/src/components/BugsComponent.vue
+++ debugger-week-7.client/src/components/BugsComponent.vue
@@ -1,9 +1,20 @@
 <template>
   <div class="BugsComponent my-3">
-    <!-- <h1>hello bugs</h1> -->
-    <h1>
-      {{ bugs.title }}
-    </h1>
+    <div class="row">
+      <h1>
+        {{ bugs.title }}
+      </h1>
+    </div>
+    <div class="row">
+      <h1>
+        {{ bugs.description }}
+      </h1>
+    </div>
+    <div class="row">
+      <h1>
+        {{ bugs.updatedAt }}
+      </h1>
+    </div>
   </div>
 </template>
 
